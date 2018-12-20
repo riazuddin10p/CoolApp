42a9ff388190528cae17bb1dee4966e1a86c8620
riazuddin10p <riaz.ahmed@tenpearls.com>
2018-12-20 11:23:01 +0500

    Add live unit testing example

---
+++ StringLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("21eda804-841b-45e1-b0b1-863cab6efaa2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UtilityLibraries/StringLibrary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLibraries
+{
+    public static class StringLibrary
+    {
+        public static bool StartsWithUpper(this string s)
+        {
+            if (String.IsNullOrWhiteSpace(s))
+                return false;
+
+            return Char.IsUpper(s[0]);
+        }
+
+        public static bool StartsWithLower(this string s)
+        {
+            if (String.IsNullOrWhiteSpace(s))
+                return false;
+
+            return Char.IsLower(s[0]);
+        }
+
+        public static bool HasEmbeddedSpaces(this string s)
+        {
+            if (String.IsNullOrWhiteSpace(s))
+                return false;
+
+            foreach (var ch in s.Trim())
+            {
+                if (ch == ' ')
+                    return true;
+            }
+            return false;
+        }
+    }
+}
